cbedd9354bf3d5024a48130ac4722b27699e009f
aurixai2026-a11y <aurix.ai.2026@gmail.com>
2026-06-04 13:29:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ const express = require("express");
 const deviceController = require("./controllers/deviceController");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+const allowedOrigins = new Set([
+  "https://aurix-dashboard.netlify.app",
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://127.0.0.1:5500"
+]);
+
+function corsMiddleware(req, res, next) {
+  const origin = req.headers.origin;
+
+  if (origin && allowedOrigins.has(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Vary", "Origin");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  }
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  return next();
+}
+
+app.use(corsMiddleware);
 app.use(express.json());
 
 app.use("/api/device", require("./routes/deviceRoutes"));
 app.get("/api/devices", deviceController.list);
 app.use("/api/user", require("./routes/userRoutes"));
 
-app.listen(3000, () => {
-  console.log("Aurix Backend Running");
+app.listen(PORT, () => {
+  console.log(`Aurix Backend Running on port ${PORT}`);
 });
