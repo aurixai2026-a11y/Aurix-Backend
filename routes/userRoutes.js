2b035399c213d65560a84bb9b27dfc1429abe4e9
aurixai2026-a11y <aurix.ai.2026@gmail.com>
2026-06-23 12:58:13 +0530

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
+const userController = require("../controllers/userController");
+
+// Public: get user details by id
+router.get("/:id", userController.getUser);
 
 module.exports = router;
